e014aa66ddbbd988306980a3ec836edd17c55661
rnd.bertrand@gmail.com <>
2014-01-21 20:48:37 +0000

    default setup after simple application wizard

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import bottle
+import os
+
+def application(environ, start_response):
+    data = "Hello World!"
+    start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+            ])
+    return iter([data])
+
